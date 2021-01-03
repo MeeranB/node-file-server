ca45a9742d62b83928716cba23611b90c0f7fc94
Meeran B <v9@live.co.uk>
2021-01-03 11:23:14 +0000

    Fix public.js to correctly serve public assets

--- workshop/handlers/public.js
+++ workshop/handlers/public.js
@@ -1,7 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+
 function publicHandler(request, response) {
-  // replace this!
-  response.writeHead(404);
-  response.end();
+  const types = {
+    html: "text/html",
+    css: "text/css",
+    js: "application/javascript",
+    jpg: "image/jpg",
+  };
+  const urlArray = request.url.split(".");
+  const extension = urlArray[1];
+  const type = types[extension];
+  response.writeHead(200, { "content-type": type });
+  const fileName = request.url.split("/")[2];
+  const filePath = path.join(__dirname, "..", "public", fileName);
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      console.log(error);
+    } else {
+      response.end(file);
+    }
+  });
 }
 
 module.exports = publicHandler;
--- workshop/practice.js
+++ workshop/practice.js
@@ -1,1 +1,10 @@
-console.log("try out the fs module here");
+const fs = require("fs");
+const path = require("path");
+
+fs.readFile(path.join(__dirname, "test.txt"), "utf-8", (error, file) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(file);
+  }
+});
